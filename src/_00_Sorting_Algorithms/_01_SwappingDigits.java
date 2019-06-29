58f2f468a10e3beed860db147b1c6ff3529135ff
League <league@Groovy.local>
2019-06-29 11:54:47 -0700

    fixed errors

--- src/_00_Sorting_Algorithms/_01_SwappingDigits.java
+++ src/_00_Sorting_Algorithms/_01_SwappingDigits.java
@@ -28,8 +28,6 @@ class _01_SwappingDigits {
 		assertEquals(5, findMiddle(m));
 	}
 
-	// 1. Complete the method so that the first two elements of the array are
-	// swapped
 	public static void swapArrayOfTwo(int[] arr) {
 		if (arr[0] < arr[1]) {
 			int value = arr[0];
@@ -38,9 +36,6 @@ class _01_SwappingDigits {
 		}
 	}
 
-	// 2. Complete the method so that it sorts the array using a bubble sort.
-	// Iterate through the array and when you find two elements that are out
-	// of order, swap them. Repeat this until the array is in order.
 	public static void sortIntArray(int[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			for (int f = 0; f < arr.length - 1; f++) {
@@ -53,20 +48,9 @@ class _01_SwappingDigits {
 		}
 	}
 
-	// 3. Complete the method so that it finds the middle number in the array.
-	// *Hint* it helps to sort it first.
-	// *Double Hint* Use the method you already wrote in step 2 to sort it
 	public static int findMiddle(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			for (int f = 0; f < arr.length - 1; f++) {
-				if (arr[f] > arr[f + 1]) {
-					int value = arr[f];
-					arr[f] = arr[f + 1];
-					arr[f + 1] = value;
-				}
-			}
-		}
-		int value = arr.length/2;
+		sortIntArray(arr);
+		int value = arr.length / 2;
 		return arr[value];
 	}
 }
