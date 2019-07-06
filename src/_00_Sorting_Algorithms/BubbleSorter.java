5ccaadcf1a5bff42f552f20a1e8c46563292b8a3
DCamaron1 <league@Mac-mini-4.local>
2019-07-06 12:09:21 -0700

    7-6-19
    Finished bubble sort

--- src/_00_Sorting_Algorithms/BubbleSorter.java
+++ src/_00_Sorting_Algorithms/BubbleSorter.java
@@ -10,7 +10,18 @@ public class BubbleSorter extends Sorter{
 	//   progress on the graph.
 	@Override
 	void sort(int[] array, SortingVisualizer display) {
-		
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length-1; j++) {
+				int x = array[j];
+				int y = array[j+1];
+				if (x>y) {
+					y = array[j];
+					array[j]= array[j+1];
+					array[j+1]=y;
+					display.updateDisplay();
+				}
+			}
+		}
 	}
 	
 }
