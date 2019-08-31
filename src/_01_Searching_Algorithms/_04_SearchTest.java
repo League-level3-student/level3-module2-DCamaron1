8bc72def81e39553879f1aca4efa41ad22519978
Your Name <you@example.com>
2019-08-31 12:13:38 -0700

    8-31-19

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -11,23 +11,31 @@ class _04_SearchTest {
 	@Test
 	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
+		String[] list = {"october", "yes", "peanut", "book", "wall", "twelve"};
+		assertEquals(2,_00_LinearSearch.linearSearch(list, "peanut"));
 	}
 
 	@Test
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		String[] list = {"october", "yes", "peanut", "book", "wall", "twelve"};
+		assertEquals(2,_00_LinearSearch.binarySearch(list, "peanut"));
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+		String[] list = {"october", "yes", "peanut", "book", "wall", "twelve"};
+		assertEquals(2,_00_LinearSearch.interpolationSearch(list, "peanut"));
 	}
 	
 	@Test
 	public void testExponentialSearch() {
 		//4. use the assertEquals method to test your exponential search method.
 		//   remember that the array must be sorted
+		String[] list = {"october", "yes", "peanut", "book", "wall", "twelve"};
+		assertEquals(2,_00_LinearSearch.exponentialSearch(list, "peanut"));
 	}
 }
