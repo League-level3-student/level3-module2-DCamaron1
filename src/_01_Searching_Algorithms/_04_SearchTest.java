30bd68d326b9f76b57c7e5450cc693a8bbb98a7a
League <league@Admins-Mac-mini.local>
2019-09-28 12:21:56 -0700

    9-28-19

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -19,23 +19,23 @@ class _04_SearchTest {
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
-		String[] list = {"october", "yes", "peanut", "book", "wall", "twelve"};
-		assertEquals(2,_00_LinearSearch.binarySearch(list, "peanut"));
+		int[] list = {1, 3, 5, 7, 10, 13};
+		assertEquals(2,_01_BinarySearch.binarySearch(list, 0, list.length-1,5));
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
-		String[] list = {"october", "yes", "peanut", "book", "wall", "twelve"};
-		assertEquals(2,_00_LinearSearch.interpolationSearch(list, "peanut"));
+		int[] list = {10, 20, 30, 40, 50, 60};
+		assertEquals(4,_02_InterpolationSearch.interpolationSearch(list, 50));
 	}
 	
 	@Test
 	public void testExponentialSearch() {
 		//4. use the assertEquals method to test your exponential search method.
 		//   remember that the array must be sorted
-		String[] list = {"october", "yes", "peanut", "book", "wall", "twelve"};
-		assertEquals(2,_00_LinearSearch.exponentialSearch(list, "peanut"));
+		String[] list = {""};
+		assertEquals(2,_03_ExponentialSearch.exponentialSearch(null, 0));
 	}
 }
